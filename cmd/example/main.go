8c54792a824c691deb2e185e42084e1f6d404a2c
#Patriot0911 <75264092+Patriot0911@users.noreply.github.com>
2024-03-03 19:47:01 +0200

    main flags and so on

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"flag"
+	"os"
+
+	lab2 "github.com/ipassternak/LAB2-archProg"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
+	inputFile       = flag.String("f", "", "File to read")
+	outputFile      = flag.String("o", "", "File to output")
 )
 
 func main() {
 	flag.Parse()
-	// handler := &lab2.ComputeHandler{
-	// <paste your streams here>
-	// }
-	// err := handler.Compute()
-	// if err != nil {
-	// 	log.Fatalf("Error: %v", err)
-	// }
+	if len(*inputExpression) < 1 && len(*inputFile) < 1 && len(*outputFile) < 1 {
+		os.Stderr.WriteString("You should provide atleast one flag as arg")
+		return
+	}
+	handler := &lab2.ComputeHandler{
+		// <paste your streams here>
+	}
+	err := handler.Compute()
+	if err != nil {
+		os.Stderr.WriteString("LOL it is test err, u r loooser")
+	}
 }
